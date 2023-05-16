b713633f743e9a72eb3f4880595c16bb1995f142
Alishah Safdari <alishahsafdari@gmail.com>
2023-05-16 21:22:30 +0430

    Add the index.html with style.css files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+// import Leaderbord from '../modules/leaderbord.js';
+// import Selector from '../modules/selector.js';
+
+// const objLeaderbard = new Leaderbord();
+// const objSelector = new Selector();
+
+// window.addEventListener('load', () => {
+//     objLeaderbard.dispaly();
+//   });
+
+// objSelector.jsForm.addEventListener('submit', (e) => {
+//     objLeaderbard.addTeam(e);
+//   });
